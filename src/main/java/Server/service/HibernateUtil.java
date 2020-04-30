9522d2a4d78c412ad20e031fbc6e299cea3c6dfb
truongHoangCong <46306829+truongHoangCong@users.noreply.github.com>
2020-04-30 14:22:49 +0700

    Edit HibernateUtil

--- src/main/java/Server/service/HibernateUtil.java
+++ src/main/java/Server/service/HibernateUtil.java
@@ -1,46 +1,89 @@
 package Server.service;
-import Server.model.DB.SongEntity;
+
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.SystemPropertyUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory = null;
+    public static Map<Class, SessionFactory> mapSession = new HashMap<>();
 
     static {
-        try{
-            loadSessionFactory();
-        }catch(Exception e){
+        try {
+            List<Class> classList = findMyTypes("Server.model.DB");
+            for (Class item : classList) {
+                mapSession.put(item, loadSessionFactory(item));
+            }
+        } catch (Exception e) {
             System.err.println("Exception while initializing hibernate util.. ");
             e.printStackTrace();
         }
     }
 
-    public static void loadSessionFactory(){
-
+    public static SessionFactory loadSessionFactory(Class clazz) {
         Configuration configuration = new Configuration();
         configuration.configure("hibernate.cfg.xml");
-        configuration.addAnnotatedClass(SongEntity.class);
+        configuration.addAnnotatedClass(clazz);
         ServiceRegistry srvcReg = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
-        sessionFactory = configuration.buildSessionFactory(srvcReg);
+        return configuration.buildSessionFactory(srvcReg);
     }
 
-    public static Session getSession() throws HibernateException {
+    public static Session getSession(Class clazzEntity) throws HibernateException {
 
-        Session retSession=null;
+        Session retSession = null;
         try {
-            retSession = sessionFactory.openSession();
-        }catch(Throwable t){
+            for (Map.Entry<Class, SessionFactory> entry : mapSession.entrySet()) {
+                if (clazzEntity.equals(entry.getKey())) {
+                    retSession = entry.getValue().getCurrentSession();
+                    break;
+                }
+            }
+        } catch (Throwable t) {
             System.err.println("Exception while getting session.. ");
             t.printStackTrace();
         }
-        if(retSession == null) {
+        if (retSession == null) {
             System.err.println("session is discovered null");
         }
-
         return retSession;
     }
+
+    public static List<Class> findMyTypes(String basePackage) throws IOException, ClassNotFoundException {
+        ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        MetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory(resourcePatternResolver);
+
+        List<Class> candidates = new ArrayList<Class>();
+        String packageSearchPath = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX +
+                resolveBasePackage(basePackage) + "/" + "**/*.class";
+        Resource[] resources = resourcePatternResolver.getResources(packageSearchPath);
+        for (Resource resource : resources) {
+            if (resource.isReadable()) {
+                MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(resource);
+                candidates.add(Class.forName(metadataReader.getClassMetadata().getClassName()));
+            }
+        }
+        return candidates;
+    }
+
+    private static String resolveBasePackage(String basePackage) {
+        return ClassUtils.convertClassNameToResourcePath(SystemPropertyUtils.resolvePlaceholders(basePackage));
+    }
+
 }
